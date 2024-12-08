0353532b90d8ed176e92ea0ba29eb687a274c41d
hig1002 <hig7463@naver.com>
2024-12-09 02:36:51 +0900

    fix : 토론방 댓글 페포 수정

--- src/main/java/database/doktalk/domain/discussionreview/repository/DiscussionReviewRepository.java
+++ src/main/java/database/doktalk/domain/discussionreview/repository/DiscussionReviewRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface DiscussionReviewRepository extends JpaRepository<DiscussionReview, Long> {
-    List<DiscussionReview> findByDiscussionId(Long discussionId);
+    List<DiscussionReview> findAllByDiscussionId(Long discussionId);
 }
